5624c31ae57907116987a67c1abc18e34ea1d122
Erik Hvattum <erik.hvattum@gmail.com>
2018-07-30 23:44:21 +0200

    initial commit. Added bootstrap files and he soure for .gitconfig and profile.ps1

---
+++ bootstrap.ps1
@@ -0,0 +1,32 @@
+#Requires -RunAsAdministrator
+Write-Host "Setting up your computer"
+#Change script excecution policy
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force;
+
+#Updating packageproviders and adding some useful PowerShell modules
+Install-PackageProvider Nuget –Force
+Install-Module Jump.Location
+Install-Module PSColors
+Install-Module posh-git
+
+#Install chocolatey
+Invoke-Expression "((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))";
+Set-Variable "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin";
+#Install applications
+chocolatey.exe install -y 7zip keepass kdiff3 conemu notepadplusplus visualstudiocode firefox dotnetcore-sdk -y;
+chocolatey.exe install git.install --params "/GitAndUnixToolsOnPath /SChannel /WindowsTerminal /NoShellIntegration"
+chocolatey.exe install docker-for-windows --pre -y
+#Create the profile.ps1 if it does not exist
+if (!(Test-Path -Path $PROFILE)){ New-Item -Path $PROFILE -ItemType File };
+#Append the awesome profile
+Get-Content profile-template.ps1 >> $PROFILE
+
+#Copy a sane .gitconfig
+Copy-Item .gitconfig ~/.gitconfig
+$GitName = Read-Host -Prompt 'Input your name'
+$GitEmail = Read-Host -Prompt 'Input your email address'
+git config --global user.name  $GitName
+git config --global user.email  $GitEmail
+
+#Set up the vscode settings sync extension. Use this to source your settings from previous installations
+code.cmd --install-extension Shan.code-settings-sync
